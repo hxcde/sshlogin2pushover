d4e0df1c37b8effbe4b7a742cba44ffe10e76f93
Yann Mölle <ymoelle@noreply.localhost>
2023-04-12 07:37:19 +0000

    „install.sh“ ändern

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo PUSHOVER_MESSAGE_TEXT=" $userkey " | cat - sshlogin.sh > temp && mv temp ss
 echo '#!/bin/bash' | cat - sshlogin.sh > temp && mv temp sshlogin.sh
 chmod +x sshlogin.sh
 chmod +x pushover.sh
-mdkir /opt/push
+mkdir /opt/push
 cp pushover.sh /opt/push/
 cp sshlogin.sh /usr/local/bin/
 echo "session optional pam_exec.so seteuid /usr/local/bin/sshlogin.sh" >> /etc/pam.d/sshd
