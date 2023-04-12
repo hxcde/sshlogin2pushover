9bc57a64c4dbcee10cada969491d882bc8ab18ca
Yann Mölle <ymoelle@noreply.localhost>
2023-04-12 07:43:47 +0000

    „uninstall.sh“ hinzufügen

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+echo starting uninstall
+rm -rf /opt/push
+rm /usr/local/bin/sshlogin.sh
+grep -v "session optional pam_exec.so seteuid /usr/local/bin/sshlogin.sh" /etc/pam.d/sshd > /etc/pam.d/tmpfile && mv tmpfile /etc/pam.d/sshd
+echo uninstalled
