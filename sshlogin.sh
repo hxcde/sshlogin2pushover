db4962e9008ab100371702d2dc03a6818536f19b
Yann Mölle <ymoelle@noreply.localhost>
2023-04-12 07:24:21 +0000

    „sshlogin.sh“ ändern

--- sshlogin.sh
+++ sshlogin.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 if [ "$PAM_TYPE" != "close_session" ]; then
         host="`hostname`"
         message="`env`"
-        sh /opt/push/pushover.sh -u [userkey] -a [apikey/token] "SSH Login: $PAM_USER from $PAM_RHOST on $host"
+        sh /opt/push/pushover.sh -u ${userkey} -a ${apikey} "SSH Login: $PAM_USER from $PAM_RHOST on $host"
 fi
